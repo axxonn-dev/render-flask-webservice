d5c7739eed8b34c28a50a570f883d8f89fab5eb2
axxonn-dev <axxonngit@gmail.com>
2025-07-11 11:24:00 -0400

    Update webservice.py

--- webservice.py
+++ webservice.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, jsonify, render_template_string
+from flask import Flask, request, jsonify
 import re
 import os
-
 app = Flask(__name__)
 
 @app.route("/tokenize", methods=["POST"])
@@ -9,27 +8,10 @@ def tokenize():
     data = request.get_json()
     text = data.get("text", "")
     tokens = simple_tokenize(text)
-    return render_tokens_html(tokens)
+    return jsonify({"tokens": tokens})
 
 def simple_tokenize(text):
     return re.findall(r'\b\w+\b', text.lower())
-
-def render_tokens_html(tokens):
-    html = """
-    <!DOCTYPE html>
-    <html>
-    <head><meta charset="UTF-8"><title>Tokens</title></head>
-    <body>
-        <h2>Tokens:</h2>
-        <ul>
-            {% for token in tokens %}
-            <li>{{ token }}</li>
-            {% endfor %}
-        </ul>
-    </body>
-    </html>
-    """
-    return render_template_string(html, tokens=tokens)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
